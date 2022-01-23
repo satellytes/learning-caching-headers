17b9efa5fa933b023333d3980aec09ccee40df25
Fabian Dietenberger <fabian.dietenberger@satellytes.com>
2022-01-23 13:01:11 +0100

    feat: add e-tag for 304 responses

--- handler.ts
+++ handler.ts
@@ -1,41 +1,74 @@
-"use strict";
-
+import { createHash } from 'crypto';
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 
-const CORS_HEADERS = {
-  'Access-Control-Allow-Headers': '*',
-  'Access-Control-Allow-Methods': '*',
-  'Access-Control-Allow-Origin': '*',
+// browser cache
+const MAX_AGE_SECONDS = 10;
+
+// cdn cache
+const S_MAXAGE_SECONDS = 60;
+
+// cdn (after s-maxage) and browser stale (after max-age) cache
+const STALE_WHILE_REVALIDATE_SECONDS = 240;
+
+/**
+ * Takes the body as object and creates a string out of it.
+ */
+const createBody = (object: any) => {
+  return JSON.stringify(object, null, 2);
 }
 
-const formatBody = (object: any) => {
-  return JSON.stringify(object, null, 2);
+/**
+ * Create an E-Tag hashed based of the body string. You might want to choose a different hashing algorithm if performance
+ * matters.
+ *
+ * Resources:
+ * - https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/ETag
+ * - https://github.com/fastify/fastify-etag
+ * - https://github.com/sindresorhus/fnv1a
+ */
+const createEtag = (body: string) => {
+  const hash = createHash('md5').update(body).digest('hex');
+  return `W/"${hash}"`
 }
 
 export const handleRequest: APIGatewayProxyHandlerV2 = async (event) => {
   console.log(`Received ${event.requestContext.http.method} request...`);
 
-  const createdDate = new Date().toISOString();
+  const body = createBody({
+    createdDate: new Date().toUTCString(),
+    'lorem-why': 'here is some string to make the response larger. this makes it simpler to differentiate cached und uncached responses',
+    'lorem-1': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    'lorem-2': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    'lorem-3': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    'lorem-4': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    'lorem-5': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    'lorem-6': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    'lorem-7': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    'lorem-8': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    'lorem-9': 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+  })
 
-  /**
-   * The max age is 1 year, which is 31536000 seconds.
-   *
-   * https://stackoverflow.com/questions/7071763/max-value-for-cache-control-header-in-http
-   */
-  const browserCacheSeconds = 10;
-  const cdnCacheSeconds = 30;
-  const cdnStaleCacheSeconds = 30;
+  const eTag = createEtag(body);
 
   return {
     statusCode: 200,
-    body: formatBody(
-      {
-        createdDate
-      },
-    ),
+    body: body,
     headers: {
-      ...CORS_HEADERS,
-      'Cache-Control': `max-age=${browserCacheSeconds}, s-maxage=${cdnCacheSeconds}`,
+      /**
+       * Cache-Control is a strong cache indicator (also Expires). E-Tag is a weak one (also Last-Modified).
+       * There should be one strong and one weak caching indicator.
+       *
+       * https://stackoverflow.com/questions/5321876/which-one-to-use-expire-header-last-modified-header-or-etags
+       */
+      'Cache-Control': `public, max-age=${MAX_AGE_SECONDS}, s-maxage=${S_MAXAGE_SECONDS}, stale-while-revalidate=${STALE_WHILE_REVALIDATE_SECONDS}`,
+      'ETag': eTag,
+
+      /**
+       * We allow CORS for all domains to make it simpler to work with the API.
+       */
+      'Access-Control-Allow-Headers': '*',
+      'Access-Control-Allow-Methods': '*',
+      'Access-Control-Allow-Origin': '*',
     }
   };
 };
